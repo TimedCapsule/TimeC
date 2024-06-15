15e8278eb30416abdf6260345f8429fce3a51eac
Darshan Vardhan Gupta <guptadarshan786@gmail.com>
2024-06-15 14:12:44 +0530

    views

--- TimeCapsule/TC/views.py
+++ TimeCapsule/TC/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
-
+from .models import TimeCapsule
 # Create your views here.
 
 def home(request):
     return render(request, "TC/home.html")
+
+def viewCapsules(request):
+    capsules = TimeCapsule.objects.all()
+    publicCapsules = [cap for cap in capsules if cap.status is 'public']
+    print(publicCapsules)
+    return HttpResponse('ok')
